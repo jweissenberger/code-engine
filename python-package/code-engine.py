7a7736cf79f94bbc1980104de3d8d299899cc9aa
Jack Weissenberger <johnaweissenberger@gmail.com>
2022-06-08 22:38:04 -0700

    slow progress

--- python-package/code-engine.py
+++ python-package/code-engine.py
@@ -33,7 +33,7 @@ class CodeEngine:
         #TODO: Add warning here that we don't take responsibility for the code that's generated for the service and that it could harm your system if you ask it to write dangerous code
     
     def generate_code(
-            self, docstrings: str, inputs:Union[str, list], test_case: Union[dict, list], 
+            self, docstrings: str, inputs:Union[str, list], test_cases: Union[dict, list], 
             func_name: str="solution", output_type: type=None, input_types: Union[type, dict]=None, 
         ) -> Solution:
         """
@@ -42,8 +42,33 @@ class CodeEngine:
         Inputs:
             Required:
                 docstrings: (string) description of what you want the code to do
-                func_args: (string or list of strings) strings for the input names of the function
-                test_case: (dict or list of dicts) test cases to verify that the output function is working properly
+                inputs: (string or list of strings) strings for the input names of the function
+                test_cases: (dict or list of dicts) test cases to verify that the output function is working properly
+                    format: 
+                        Single input argument, single test case: 
+                        {
+                            "name_of_your_input": test_input_object,
+                            "output": output_object_the_code_should_create
+                        }
+
+                        Mulitple arguments, single test case:
+                        {
+                            "name_of_your_first_input": test_input_object,
+                            "name_of_your_second_input": test_input_object,
+                            "output": output_object_the_code_should_create
+                        }
+
+                        Mulitple arguments, multiple test cases:
+                        [
+                            {"name_of_your_first_input": test_input_object_1,
+                            "name_of_your_second_input": test_input_object_1,
+                            "output": output_object_the_code_should_create},
+
+                            {"name_of_your_first_input": test_input_object_2,
+                            "name_of_your_second_input": test_input_object_2,
+                            "output": output_object_the_code_should_create}
+                        ]
+          
             Optional:
                 func_name: (string) name of the output function that's going to solve your problem (this can help the service figure out the code to write)
                 output_type: (type) type of the output of the function (this can help the service figure out the code to write)
@@ -52,8 +77,13 @@ class CodeEngine:
             Solution object
         """
         assert type(inputs) is list or type(inputs) is str, "inputs must be a string or a list of strings"
+        if type(inputs) is str:
+            inputs = [inputs] # can now assume inputs will always be in a list
+        if type(test_cases) is dict:
+            test_cases = [test_cases] # can now assume test_cases will be in a list
+
         examples = self._find_examples(docstrings)
-        prompts = self._generate_prompts(examples, docstrings, inputs, test_case, func_name, output_type, input_types)
+        prompts = self._generate_prompts(examples, docstrings, inputs, test_cases, func_name, output_type, input_types)
         passed, solution, results = self._generate_and_test_code(prompts, inputs,)
 
         if passed:
@@ -64,24 +94,43 @@ class CodeEngine:
 
 
 
-    def _find_examples(self, prompt: str, num_examples: int=10) -> list:
+    def _find_examples(self, docstrings: str, num_examples: int=10) -> list:
         """
         Searches example database for most relevant examples to aid in code generation
         """
         # currently hit local running bm25 database
         url = "http://127.0.0.1:8000/get_answers"
-        data = {"query": prompt, "n_answers": num_examples}
+        data = {"query": docstrings, "n_answers": num_examples}
         r = requests.post(url=url, json=data)
         answers = r.json()["answers"]
         raise answers
 
 
-    def _generate_prompts(self, examples, docstrings, inputs, test_case, func_name, output_type, arg_types) -> list:
-        if type(inputs) is str:
-            # if all of the test cases for this argument have the same type 
-            function = f"def {func_name}({inputs})"
-        a = 2
+    def _generate_prompts(self, examples, docstrings, inputs, test_cases, func_name, output_type, input_types) -> list:
+        
+        if not input_types:
+            input_types = self._infer_input_types(inputs, test_cases)
+
+        function = f"def {func_name}({inputs})"
+        
         raise NotImplementedError
+    
+    def _infer_input_types(self, inputs, test_cases):
+        
+        input_types = {}
+        for inp in inputs:
+            # make sure the type is the same across all test cases
+            all_same = True
+            if not test_cases.get(inp):
+                return False # this is for the edge case of an option argument, I am getting to far ahead of myself
+
+            for case in test_cases
+
+            # if they're not all the same, return False and don't do type hints
+            if not all_same:
+                return False
+
+
     
     def _generate_and_test_code(self, prompts: list, inputs: list, outputs: list, max_tries: int=200):
         """
